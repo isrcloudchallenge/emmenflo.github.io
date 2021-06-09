4bd24f7d6149b4962ace6cc8148f63a80b26fe43
isrcloudchallenge <martin.jahr@isr.de>
2021-06-09 10:50:48 +0000

    deploy: isrcloudchallenge/emmenflo@83a15c4e0c0697b5e501bd0f33c19e110d43dd07

--- en.search.min.f2e3800a34fa2786a68d6c8c025b18967af9db6ad7738db8c3654056f7120590.js
+++ en.search.min.f2e3800a34fa2786a68d6c8c025b18967af9db6ad7738db8c3654056f7120590.js
@@ -1,4 +1,4 @@
-'use strict';(function(){const searchDataURL='/en.search-data.min.eaccad92e24ae09ab45a3b7c71a208fafa71559d86553993f591ce25fadc5d4a.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+'use strict';(function(){const searchDataURL='/en.search-data.min.b86782460db4ee1290374f39ef21004a88ccb7594a4cb5ca0b18421bc8e693bf.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
 input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
